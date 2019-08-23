2abece71506a222d7c76a5897e7cc6d578899cba
Lydia Sanyu <lydiansanyu@gmail.com>
2019-08-23 13:11:35 +0300

    Add image upload with active storage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,13 +62,11 @@ class ItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :price)
+      params.require(:item).permit(:name, :description, :price, images: [])
     end
 end
